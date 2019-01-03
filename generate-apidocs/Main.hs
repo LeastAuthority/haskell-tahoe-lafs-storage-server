4547940b6558cb22eda8c83c92c8beef19584bce
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-01-03 10:48:43 -0500

    Add a tool for generating html documentation

---
+++ generate-apidocs/Main.hs
@@ -0,0 +1,56 @@
+module Main where
+
+import Prelude hiding
+  ( putStrLn
+  )
+
+import Data.Text
+  ( Text
+  , unpack
+  , pack
+  )
+
+import Data.Text.IO
+  ( putStrLn
+  )
+
+import Text.Pandoc.Class
+  ( PandocIO
+  , runIOorExplode
+  )
+
+import Text.Pandoc.Writers.HTML
+  ( writeHtml5String
+  )
+
+import Text.Blaze.Html.Renderer.Pretty
+  ( renderHtml
+  )
+
+import Servant.Docs
+  ( docs
+  )
+
+import Servant.Docs.Pandoc
+  ( pandoc
+  )
+
+import Data.Default
+  ( def
+  )
+
+-- Import for instances only.
+import TahoeLAFS.Storage.APIDocs
+  (
+  )
+
+import TahoeLAFS.Storage.API
+  ( api
+  )
+
+main :: IO ()
+main = do
+  let doc = pandoc $ docs $ api
+  let markup = writeHtml5String def doc
+  html <- runIOorExplode markup
+  putStrLn html
